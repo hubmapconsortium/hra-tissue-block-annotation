6c6085aceaa0aab82b7325c6f759de8dc3ab6c2f
Bruce W. Herr II <bherr@iu.edu>
2025-06-09 10:24:39 -0400

    Update model download script

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -e
 
+VERSION=v2.3
+off_url=https://cdn.humanatlas.io/hra-glb-off-releases/hra-glb-off-data.${VERSION}.zip
+
 rm -rf model
 mkdir -p model
-
-off_url=https://cdn.humanatlas.io/hra-glb-off-releases/hra-glb-off.v2.3.zip
-as_url=https://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/mesh-collision/anatomical-structures.csv
-patch_url=https://grlc.io/api-git/hubmapconsortium/ccf-grlc/subdir/mesh-collision/placement-patches.csv
-
-curl $off_url -o model/hra-glb-off.zip
-unzip model/hra-glb-off.zip -d model/all_preprocessed_off_models_cgal
-rm -f model/hra-glb-off.zip
-
-curl $as_url -o model/asct-b-grlc.csv
-curl $patch_url -o model/reference-organ-grlc.csv
+curl $off_url -o hra-glb-off-data.zip
+unzip -q hra-glb-off-data.zip -d model
+rm -f hra-glb-off-data.zip
